c594d96535f88211da9041995684aceac10ab1f2
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-05-09 23:35:55 +0530

    Initialised the files and setup for the environment

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-bold text-violet-600 bg-black min-h-screen min-w-full pt-5">
+        <span className="font-bold text-[20px]">TODO LIST APP</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
